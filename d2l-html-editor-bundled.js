5041b671747ff454767abf56b94fe124d5238db9
Vincent Wong <Vincent.Wong@D2L.com>
2018-03-06 13:04:08 -0800

    Address Code review comments

--- d2l-html-editor-bundled.js
+++ d2l-html-editor-bundled.js
@@ -1476,22 +1476,10 @@ Polymer({
 	is: 'd2l-html-editor',
 
 	behaviors: [
-		window.D2LHtmlEditor.PolymerBehaviors.InsertStuff,
-		window.D2LHtmlEditor.PolymerBehaviors.Image,
-		window.D2LHtmlEditor.PolymerBehaviors.Link,
-		window.D2LHtmlEditor.PolymerBehaviors.TextStyleRollup,
-		window.D2LHtmlEditor.PolymerBehaviors.FormatRollup,
-		window.D2LHtmlEditor.PolymerBehaviors.InsertRollup,
-		window.D2LHtmlEditor.PolymerBehaviors.EquationEditor,
-		window.D2LHtmlEditor.PolymerBehaviors.Code,
-		window.D2LHtmlEditor.PolymerBehaviors.ReplaceString,
-		window.D2LHtmlEditor.PolymerBehaviors.FontFamily,
-		window.D2LHtmlEditor.PolymerBehaviors.Attributes,
-		window.D2LHtmlEditor.PolymerBehaviors.Preview,
-		window.D2LHtmlEditor.PolymerBehaviors.XsplConverter,
-		window.D2LHtmlEditor.PolymerBehaviors.Filter,
-		window.D2LHtmlEditor.PolymerBehaviors.Placeholder,
-		window.D2LHtmlEditor.PolymerBehaviors.Fullpage
+		/**
+		 * Do not place plugin behaviors here
+		 * Place plugin behaviors in the _getPluginBehavior function
+		 */
 	],
 
 	/**
