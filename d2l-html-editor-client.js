32d46bf5bb15ad250ce93da07cc636685f6481db
Martin Bayly <Martin.Bayly@D2L.com>
2018-10-16 14:37:32 -0700

    add basic support for insert stuff

--- d2l-html-editor-client.js
+++ d2l-html-editor-client.js
@@ -1,11 +1,55 @@
 (function() {
 	'use strict';
+	/*global tinymce:true */
 
 	function request(type) {
 		switch (type) {
 			case 'valenceHost':
 				return Promise.resolve(window.location.origin);
 		}
+	}
+
+	function insertStuff() {
+		return Promise.resolve({
+			config: function() {
+				return Promise.resolve({
+					isEnabled: true
+				});
+			},
+			click: function(openerId) {
+				return new Promise(function(resolve, reject) {
+					var nav = new D2L.Nav();
+					var location = new D2L.LP.Web.Http.UrlLocation('/d2l/common/dialogs/isf/selectItem.d2l')
+						.WithQueryString('ou', nav.GetParam('ou'))
+						.WithQueryString('filterMode', 'Strict');
+					var openEvent = D2L.LP.Web.UI.Legacy.MasterPages.Dialog.Open(
+						new D2L.LP.Web.UI.Html.AbsoluteHtmlId.Create(openerId),
+						location,
+						'GetSelectedItem',
+						'', /* resizeCallback*/
+						'itemSource',
+						975,
+						650,
+						tinymce.EditorManager.i18n.translate('Close'),
+						[{
+							IsEnabled: true,
+							IsPrimary: true,
+							Key: 'BTN_next',
+							Param: 'next',
+							ResponseType: 1,
+							Text: tinymce.EditorManager.i18n.translate('Next')
+						}], /* buttons */
+						false /* forceTriggerOnCancel */
+					);
+					openEvent.AddListener(function(result) {
+						resolve(result);
+					});
+					openEvent.AddReleaseListener(function() {
+						reject();
+					});
+				});
+			}
+		});
 	}
 
 	function convertToViewableHtml() {
@@ -41,6 +85,8 @@
 		switch (serviceType) {
 			case 'convert-to-viewable-html':
 				return convertToViewableHtml();
+			case 'fra-html-editor-isf':
+				return insertStuff();
 			default:
 				return Promise.resolve({
 					config: function() {
