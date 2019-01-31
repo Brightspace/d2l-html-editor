3991a9a0e9e2463c46c767b476759c4cc68df786
Margaret Dulat <Margaret.Dulat@D2L.com>
2019-01-31 10:19:59 -0800

    use d2l-colors

--- d2l-placeholder-styles.js
+++ d2l-placeholder-styles.js
@@ -1,10 +1,11 @@
+import 'd2l-colors/d2l-colors.js';
 const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = /*html*/`<dom-module id="d2l-placeholder-styles">
   <template>
 	<style>
 		:host ::slotted(.html-editor-container-hover) {
-			border-color: #006fbf; /*$d2l-color-celestine*/
+			border-color: var(--d2l-color-celestine);
 			border-width: 2px;
 			outline-width: 0;
 			padding: calc(0.4rem - 1px) calc(1rem - 1px)
@@ -14,7 +15,7 @@ $_documentContainer.innerHTML = /*html*/`<dom-module id="d2l-placeholder-styles"
 			position: absolute;
 			top: 0;
 			left: 0;
-			color: #b9c2d0; /*d2l-colors-pressicus*/
+			color:var(--d2l-color-pressicus);
 			font-size: 0.8rem;
 			line-height: 1.2rem;
 			padding: 0.4rem 1rem;
