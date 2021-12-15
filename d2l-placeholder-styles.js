7220bfd547a788a0da16c820b205532000118d2b
Devon Powell <Devon.Powell@D2L.com>
2021-12-15 12:16:10 -0500

    fix: remove unneeded imports (#198)
    The `d2l-icons` package wasn't used and there were 2 colors that were used from
    `d2l-colors` but they had already been hard-coded elsewhere so followed suite to
    eliminate need for it.

--- d2l-placeholder-styles.js
+++ d2l-placeholder-styles.js
@@ -1,11 +1,10 @@
-import 'd2l-colors/d2l-colors.js';
 const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = /*html*/`<dom-module id="d2l-placeholder-styles">
   <template>
 	<style>
 		:host ::slotted(.html-editor-container-hover) {
-			border-color: var(--d2l-color-celestine);
+			border-color: #006fbf; /*$d2l-color-celestine*/
 			border-width: 2px;
 			outline-width: 0;
 			padding: calc(0.4rem - 1px) calc(1rem - 1px)
@@ -15,7 +14,7 @@ $_documentContainer.innerHTML = /*html*/`<dom-module id="d2l-placeholder-styles"
 			position: absolute;
 			top: 0;
 			left: 0;
-			color:var(--d2l-color-pressicus);
+			color: #b9c2d0; /*d2l-colors-pressicus*/
 			font-size: 0.8rem;
 			line-height: 1.2rem;
 			padding: 0.4rem 1rem;
